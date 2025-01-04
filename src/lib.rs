04a5f4f93fa328659fd5591d1eae6538bcccc35c
balu <jotanlima@gmail.com>
2025-01-04 18:19:17 -0300

    add convert_str_to_i32 function

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,12 @@ where
     n % T::from(2).unwrap() == T::zero()
 }
 
+pub fn convert_str_to_i32(str: &str) -> i32 {
+    str.trim()
+        .parse::<i32>()
+        .expect("Failed to convert str to i32")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -49,6 +55,35 @@ mod tests {
         fn should_return_false_when_given_an_false_number() {
             let result = is_even(3);
             assert!(!result);
+        }
+    }
+
+    mod convert_str_to_i32_tests {
+        use std::panic;
+
+        use super::*;
+
+        #[test]
+        fn should_return_value_converted_when_given_a_number() {
+            let result = convert_str_to_i32("2");
+            assert_eq!(result, 2);
+        }
+
+        #[test]
+        fn should_panic_parse_int_error_when_conversion_fails() {
+            let result = panic::catch_unwind(|| {
+                convert_str_to_i32("three");
+            });
+
+            assert!(result.is_err(), "Expected a panic but it didn't occur.");
+
+            if let Err(err) = result {
+                let err_message = err.downcast_ref::<String>().unwrap();
+                assert_eq!(
+                    err_message,
+                    "Failed to convert str to i32: ParseIntError { kind: InvalidDigit }"
+                );
+            }
         }
     }
 }
