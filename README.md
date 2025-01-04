7151f7433d83f6a605bf867cf9635cb32023369c
balu <jotanlima@gmail.com>
2025-01-04 18:53:23 -0300

    improve convert function to use generic

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Lazy helpers
 
-| Name                 | Description                                |
-|----------------------|--------------------------------------------|
-| `is_odd`             | Returns true when receive the odd number.  |
-| `is_even`            | Returns true when receive the even number. |
-| `convert_str_to_i32` | Convert str to i32. |
+| Name                 | Description                                   |
+|----------------------|-----------------------------------------------|
+| `is_odd`             | Returns true when receive the odd number.     |
+| `is_even`            | Returns true when receive the even number.    |
+| `convert_str_to_int` | Convert str to int (i8, i16, i32, i64, i128). |
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use num::PrimInt;
 
 /// Returns true when receive the odd number.
@@ -16,11 +18,15 @@ where
     n % T::from(2).unwrap() == T::zero()
 }
 
-/// Returns data converted from &str to i32
-pub fn convert_str_to_i32(str: &str) -> i32 {
+/// Returns data converted from &str to int
+pub fn convert_str_to_int<T>(str: &str) -> T
+where
+    T: FromStr,
+    <T as FromStr>::Err: std::fmt::Debug,
+{
     str.trim()
-        .parse::<i32>()
-        .expect("Failed to convert str to i32")
+        .parse::<T>()
+        .expect("Failed to convert str to int")
 }
 
 #[cfg(test)]
@@ -59,21 +65,45 @@ mod tests {
         }
     }
 
-    mod convert_str_to_i32_tests {
+    mod convert_str_to_int_tests {
         use std::panic;
 
         use super::*;
 
         #[test]
-        fn should_return_value_converted_when_given_a_number() {
-            let result = convert_str_to_i32("2");
-            assert_eq!(result, 2);
+        fn should_return_value_converted_when_given_a_i8() {
+            let result: i8 = convert_str_to_int("127");
+            assert_eq!(result, 127);
+        }
+
+        #[test]
+        fn should_return_value_converted_when_given_a_i16() {
+            let result: i32 = convert_str_to_int("32767");
+            assert_eq!(result, 32767);
+        }
+
+        #[test]
+        fn should_return_value_converted_when_given_a_i32() {
+            let result: i32 = convert_str_to_int("2147483647");
+            assert_eq!(result, 2147483647);
+        }
+
+        #[test]
+        fn should_return_value_converted_when_given_a_i64() {
+            let result: i64 = convert_str_to_int("9223372036854775807");
+            assert_eq!(result, 9223372036854775807);
+        }
+
+        #[test]
+        fn should_return_value_converted_when_given_a_i128() {
+            let result: i128 = convert_str_to_int("170141183460469231731687303715884105727");
+            assert_eq!(result, 170141183460469231731687303715884105727);
         }
 
         #[test]
         fn should_panic_parse_int_error_when_conversion_fails() {
             let result = panic::catch_unwind(|| {
-                convert_str_to_i32("three");
+                convert_str_to_int::<i32>("three");
             });
 
             assert!(result.is_err(), "Expected a panic but it didn't occur.");
@@ -82,7 +112,7 @@ mod tests {
                 let err_message = err.downcast_ref::<String>().unwrap();
                 assert_eq!(
                     err_message,
-                    "Failed to convert str to i32: ParseIntError { kind: InvalidDigit }"
+                    "Failed to convert str to int: ParseIntError { kind: InvalidDigit }"
                 );
             }
         }
